f9b89da38cda4c9bedc1b6f494c01dc12e5df50a
pyroinfernia <daniel@tbsigma.org>
2016-04-12 01:26:45 -0500

    Added files via upload

---
+++ prototypes/recipe.lua
@@ -0,0 +1,8 @@
+data.raw["recipe"]["roboport"].ingredients =
+    {
+	  {"steel-plate", 45},
+      {"iron-gear-wheel", 45},
+      {"advanced-circuit", 45},
+	  {"processing-unit", 3}
+    }
+data.raw["recipe"]["roboport"].energy_required = 20
